504d2def683eb6443485e9e64c8addc758bf11a8
kevin <yundan_wy@126.com>
2022-06-17 10:01:46 +0800

    初始化项目

---
+++ src/main/java/com/kevin/interview/concurrent/AlternateCode.java
@@ -0,0 +1,17 @@
+package com.kevin.interview.concurrent;
+
+/**
+ * 多线程多次循环打印ABC
+ * <p>
+ * 思路：借助ReentrantLock 和 Condition实现
+ *
+ * @author kevin
+ */
+public class AlternateCode {
+
+
+  public static void main(String[] args) {
+
+  }
+
+}
